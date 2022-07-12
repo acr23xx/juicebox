cbe9dc42e187c35a26c0df8773e17582f20f8c23
Comptonvz <comptonmain@hotmail.com>
2022-07-12 14:01:53 -0700

    worked on seed

--- db/index.js
+++ db/index.js
@@ -5,3 +5,19 @@ const client = new Client("postgres://localhost:5432/juicebox-dev");
 module.exports = {
   client,
 };
+
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username 
+    FROM users;
+  `
+  );
+
+  return rows;
+}
+
+// and export them
+module.exports = {
+  client,
+  getAllUsers,
+};
--- db/seed.js
+++ db/seed.js
@@ -2,6 +2,7 @@
 
 // grab our client with destructuring from the export in index.js
 const { client } = require("./index");
+const { username, password } = require("./ ");
 
 async function testDB() {
   try {
@@ -17,3 +18,69 @@ async function testDB() {
 }
 
 testDB();
+
+const { client, getAllUsers } = require("./index");
+
+async function dropTables() {
+  try {
+    console.log("Starting to drop tables...");
+
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `);
+
+    console.log("Finished dropping tables!");
+  } catch (error) {
+    console.error("Error dropping tables!");
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+      );
+    `);
+
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error;
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function testDB() {
+  try {
+    console.log("Starting to test database...");
+
+    const users = await getAllUsers();
+    console.log("getAllUsers:", users);
+
+    console.log("Finished database tests!");
+  } catch (error) {
+    console.error("Error testing database!");
+    throw error;
+  }
+}
+
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
