b992df1dcc3f969fae423ef413d527f2b30bd550
acr23xx <alexramirez.312@gmail.com>
2022-07-12 13:25:51 -0700

    move into node

---
+++ db/seed.js
@@ -0,0 +1,19 @@
+// inside db/seed.js
+
+// grab our client with destructuring from the export in index.js
+const { client } = require("./index");
+
+async function testDB() {
+  try {
+    client.connect();
+
+    const { rows } = await client.query(`SELECT * FROM users;`);
+    console.log(rows);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
+
+testDB();
