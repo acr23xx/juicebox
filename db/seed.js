280683b090d21612da7919fb34db0e595ba2c895
Comptonvz <comptonmain@hotmail.com>
2022-07-12 14:10:47 -0700

    minor detail fix

--- db/seed.js
+++ db/seed.js
@@ -1,24 +1,6 @@
 // inside db/seed.js
 
 // grab our client with destructuring from the export in index.js
-const { client } = require("./index");
-const { username, password } = require("./ ");
-
-async function testDB() {
-  try {
-    client.connect();
-
-    const { rows } = await client.query(`SELECT * FROM users;`);
-    console.log(rows);
-  } catch (error) {
-    console.error(error);
-  } finally {
-    client.end();
-  }
-}
-
-testDB();
-
 const { client, getAllUsers } = require("./index");
 
 async function dropTables() {
