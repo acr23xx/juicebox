99a0e88137f2cec00502d87262f829c4bd57455c
acr23xx <alexramirez.312@gmail.com>
2022-07-13 09:26:44 -0700

    part 1

--- db/index.js
+++ db/index.js
@@ -30,27 +30,30 @@ async function getAllUsers() {
 
 async function createUser({ username, password }) {
   try {
-    const result = await client.query(
+    const { rows } = await client.query(
       `
-      INSERT INTO users(username, password)
-      VALUES ($1, $2);
+      INSERT INTO users(username, password) 
+      VALUES($1, $2) 
+      ON CONFLICT (username) DO NOTHING 
+      RETURNING *;
     `,
       [username, password]
     );
 
-    return result;
+    return rows;
   } catch (error) {
     throw error;
   }
 }
 
-// later
-module.exports = {
-  // add createUser here!
-};
+// // later
+// module.exports = {
+//   // add createUser here!
+// };
 
 // and export them
 module.exports = {
   client,
   getAllUsers,
+  createUser,
 };
