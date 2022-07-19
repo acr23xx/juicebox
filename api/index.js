2e7a23dc2f913b5abd04f5cff3d93eab3954c577
acr23xx <alexramirez.312@gmail.com>
2022-07-19 10:28:09 -0700

    final

--- api/index.js
+++ api/index.js
@@ -1,17 +1,10 @@
+// api/index.js
 const express = require("express");
 const apiRouter = express.Router();
-
-// api/index.js
-// Before we start attaching our routers
 
 const jwt = require("jsonwebtoken");
 const { getUserById } = require("../db");
 const { JWT_SECRET } = process.env;
-
-// jwt.sign({}, process.env.JWT_SECRET);
-// res.send({
-//   id: user.id,
-// });
 
 // set `req.user` if possible
 apiRouter.use(async (req, res, next) => {
@@ -30,6 +23,11 @@ apiRouter.use(async (req, res, next) => {
       if (id) {
         req.user = await getUserById(id);
         next();
+      } else {
+        next({
+          name: "AuthorizationHeaderError",
+          message: "Authorization token malformed",
+        });
       }
     } catch ({ name, message }) {
       next({ name, message });
@@ -50,8 +48,6 @@ apiRouter.use((req, res, next) => {
   next();
 });
 
-// Attach routers below here
-
 const usersRouter = require("./users");
 apiRouter.use("/users", usersRouter);
 
@@ -59,46 +55,10 @@ const postsRouter = require("./posts");
 apiRouter.use("/posts", postsRouter);
 
 const tagsRouter = require("./tags");
-const res = require("express/lib/response");
-const { token } = require("morgan");
-const { user } = require("pg/lib/defaults");
 apiRouter.use("/tags", tagsRouter);
 
-apiRouter.use(async (req, res, next) => {
-  const prefix = "Bearer ";
-  const auth = req.headers["Authorization"];
-
-  if (!auth) {
-    next(); // don't set req.user, no token was passed in
-  }
-
-  if (auth.startsWith(prefix)) {
-    // recover the token
-    const token = auth.slice(prefix.length);
-    try {
-      // recover the data
-      const { id } = jwt.verify(data, "secret message");
-
-      // get the user from the database
-      const user = await getUserById(id);
-      // note: this might be a user or it might be null depending on if it exists
-
-      // attach the user and move on
-      req.user = user;
-
-      next();
-    } catch (error) {
-      // there are a few types of errors here
-    }
-  }
-});
-
-// all routers attached ABOVE here
 apiRouter.use((error, req, res, next) => {
-  res.send({
-    name: error.name,
-    message: error.message,
-  });
+  res.send(error);
 });
 
 module.exports = apiRouter;
--- api/tags.js
+++ api/tags.js
@@ -1,18 +1,45 @@
 const express = require("express");
 const tagsRouter = express.Router();
-const { getAllTags } = require("../db");
 
-tagsRouter.use((req, res, next) => {
-  console.log("A request is being made to /tags");
+const { getAllTags, getPostsByTagName } = require("../db");
 
-  next();
+tagsRouter.get("/", async (req, res, next) => {
+  try {
+    const tags = await getAllTags();
+
+    res.send({
+      tags,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
-tagsRouter.get("/", async (req, res) => {
-  const tags = await getAllTags();
-  res.send({
-    tags,
-  });
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  let { tagName } = req.params;
+
+  // decode %23happy to #happy
+  tagName = decodeURIComponent(tagName);
+
+  try {
+    const allPosts = await getPostsByTagName(tagName);
+
+    const posts = allPosts.filter((post) => {
+      if (post.active) {
+        return true;
+      }
+
+      if (req.user && req.user.id === post.author.id) {
+        return true;
+      }
+
+      return false;
+    });
+
+    res.send({ posts });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = tagsRouter;
